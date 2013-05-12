985c1ab1bf11a40c5e2394d689a37487a7351629
Moto Ishizawa <summerwind.jp@gmail.com>
2013-05-12 18:54:10 +0900

    Added unit test files

---
+++ test/client.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+var stun   = require('../lib');
+
+var port = 19302;
+var host = 'stun.l.google.com';
+
+module.exports = {
+    "Success": function (test) {
+        var client = stun.connect(port, host);
+
+        client.on('response', function(packet){
+            client.close();
+
+            var method = stun.method.RESPONSE_S;
+            var attr   = stun.attribute.MAPPED_ADDRESS;
+
+            test.equal(packet.class, 1);
+            test.equal(packet.method, method);
+            test.equal(packet.attrs[attr].family, 4);
+            test.notEqual(packet.attrs[attr].port, null);
+            test.notEqual(packet.attrs[attr].address, null);
+            
+            test.done();
+        });
+
+        client.request();
+    }
+};
